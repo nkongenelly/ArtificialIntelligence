00633a13481bb8d360c2bd1f84e976fd96b0f1bc
nkongenelly <nkongenelly94@gmail.com>
2021-12-22 12:28:06 +0300

    feed forward with activation function a rectified linear function

--- AICES/test.py
+++ AICES/test.py
@@ -1,8 +1,29 @@
 import numpy as np
 
+np.random.seed(0)
 inputs = [[1, 2, 3, 2.5],
             [2.0, 5.0, -1.0, 2.0],
             [-1.5, 2.7, 3.3, -0.8]]
+
+def create_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)
+        t = np.linspace(class_number * 4, (class_number + 1) * 4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r * np.sin(t * 2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y 
+
+import matplotlib.pyplot as plt
+
+print("here")
+X, y = create_data(100,3)
+
+plt.scatter(X[:,0], X[:,1], c=y, s=40, cmap=plt.cm.Spectral)
+plt.show()
+
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -12,11 +33,21 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights)
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0,inputs)
 
-layer1.forward(inputs)
-print(layer1.output.shape)
-layer2.forward(layer1.output)
-print(layer2.weights.shape)
-print(layer2.output)
+
+layer1 = Layer_Dense(2,5)
+activation1 = Activation_ReLU()
+layer1.forward(X)
+print(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
+# layer2 = Layer_Dense(5,2)
+
+# layer1.forward(inputs)
+# print(layer1.output.shape)
+# layer2.forward(layer1.output)
+# print(layer2.weights.shape)
+# print(layer2.output)
