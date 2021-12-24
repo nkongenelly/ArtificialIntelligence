9c925827b7e301c17f50e332acbddf54e7443c27
nkongenelly <nkongenelly94@gmail.com>
2021-12-24 08:43:00 +0300

    calculating loss using categorical cross-entropy using math not numpy

--- AICES/test2.py
+++ AICES/test2.py
@@ -1,14 +1,25 @@
-import numpy as np
+# import numpy as np
+import math 
 
-layer_outputs = [[4.8, 1.21, 2.385],
-				 [8.9, -1.81, 0.2],
-				 [1.41, 1.051, 0.026]]
-# exponential capping overflow
-exp_values = np.exp(layer_outputs - np.max(layer_outputs, axis=1, keepdims=True))
-# print(exp_values)
+softmax_output = [0.7,0.1,0.2]
+target_output = [1,0,0]
 
-# normalization
-norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+loss = -(math.log(softmax_output[0]) * target_output[0] + 
+		math.log(softmax_output[1]) * target_output[1] + 
+		math.log(softmax_output[2]) * target_output[2])
 
-print(norm_values)
+print(loss)
+print(-math.log(0.7))
+
+# layer_outputs = [[4.8, 1.21, 2.385],
+# 				 [8.9, -1.81, 0.2],
+# 				 [1.41, 1.051, 0.026]]
+# # exponential capping overflow
+# exp_values = np.exp(layer_outputs - np.max(layer_outputs, axis=1, keepdims=True))
+# # print(exp_values)
+
+# # normalization
+# norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+# print(norm_values)
 
