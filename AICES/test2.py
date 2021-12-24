b6f16bebaefbbbc64a4315eb0f45c3fabb9a3e6b
nkongenelly <nkongenelly94@gmail.com>
2021-12-24 07:28:39 +0300

    softmax activation function for output layer with numpy for matrix input and capping exponentials

--- AICES/test2.py
+++ AICES/test2.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-layer_outputs = [4.8, 1.21, 2.385]
-
-exp_values = np.exp(layer_outputs)
+layer_outputs = [[4.8, 1.21, 2.385],
+				 [8.9, -1.81, 0.2],
+				 [1.41, 1.051, 0.026]]
+# exponential capping overflow
+exp_values = np.exp(layer_outputs - np.max(layer_outputs, axis=1, keepdims=True))
+# print(exp_values)
 
 # normalization
-norm_values = exp_values / np.sum(exp_values)
+norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
 print(norm_values)
 
