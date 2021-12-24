01dcc71b4d13fb73abfa75303dd4412c2c92f853
nkongenelly <nkongenelly94@gmail.com>
2021-12-24 07:22:11 +0300

    softmax activation function for output layer with numpy for vector input

--- AICES/test2.py
+++ AICES/test2.py
@@ -1,21 +1,11 @@
-import math
+import numpy as np
 
 layer_outputs = [4.8, 1.21, 2.385]
 
-E = math.e
-
-exp_values = []
-
-# exponential
-for outputs in layer_outputs:
-	exp_values.append(E**outputs)
+exp_values = np.exp(layer_outputs)
 
 # normalization
-norm_base = sum(exp_values)
-norm_values = []
-
-for value in exp_values:
-	norm_values.append(value / norm_base);
+norm_values = exp_values / np.sum(exp_values)
 
 print(norm_values)
 
