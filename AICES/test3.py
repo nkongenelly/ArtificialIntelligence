b553e52dbaf7a4c6ca4be7929c0767c076c28ad2
nkongenelly <nkongenelly94@gmail.com>
2021-12-25 09:40:54 +0300

    Started the neural networks in 9 steps example

--- AICES/test3.py
+++ AICES/test3.py
@@ -7,6 +7,14 @@ np.random.seed(42) # for reproducibility
 
 # 2. DATA GENERATION
 X_num_row, X_num_col = [2, 10000] # Row is no. of feature, col is no. of datum points
-X_raw = np.random.rand(X_num_row, X_num_col) * 100
+# Rand generates values between 0 andd 1, we multiply by 100 to make the max 100
+X_raw = np.random.rand(X_num_row, X_num_col) * 100 
+# print(X_raw[0,:].shape)
 
-print(X_raw[:5])
+
+# for input a and b, output is a+b, a-b and |a-b|
+y_raw = np.concatenate(([(X_raw[0,:] + X_raw[1,:])],
+						[(X_raw[0,:] - X_raw[1,:])],
+						np.abs([(X_raw[0,:] - X_raw[1,:])]))) 
+y_num_row, y_num_col = y_raw.shape
+# print(y_raw.shape)
