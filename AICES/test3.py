2303cd20facad5bc1d36c16930e202a0493ced87
nkongenelly <nkongenelly94@gmail.com>
2021-12-28 11:05:36 +0300

    neural network construction sample

--- AICES/test3.py
+++ AICES/test3.py
@@ -61,8 +61,48 @@ X_test = np.array([standardize(X_raw_test[row,:], X_scalers[row]) for row in ran
 y_test = np.array([standardize(y_raw_test[row,:], y_scalers[row]) for row in range(y_num_row)])
 
 # check if data has been standardized
-print([X_train[row,:].mean() for row in range(X_num_row)])
-print([X_train[row,:].std() for row in range(X_num_row)])
+# print([X_train[row,:].mean() for row in range(X_num_row)])
+# print([X_train[row,:].std() for row in range(X_num_row)])
 
-print([y_train[row,:].mean() for row in range(y_num_row)])
-print([y_train[row,:].std() for row in range(y_num_row)])
+# print([y_train[row,:].mean() for row in range(y_num_row)])
+# print([y_train[row,:].std() for row in range(y_num_row)])
+
+# 5. NEURAL NETWORK CONSTRUCTION
+class layer:
+	def __init__(self, layer_index, is_output, input_dim, output_dim, activation):
+		self.layer_index = layer_index # zero indiccates input layer
+		self.is_output = is_output # true indicates output layer
+		self.innput_dim = input_dim
+		self.output_dim = output_dim
+		self.activation = activation
+
+		# the multiplication constant is sorta arbitrary
+		if layer_index !=0:
+
+			self.W = np.random.randn(output_dim, input_dim) * np.sqrt(2/input_dim)
+			self.b = np.random.randn(output_dim, 1) * np.sqrt(2/input_dim)
+
+layers_dim = [X_num_row, 4, 4, y_num_row] # input layer --- hidden layers --- out[ut layers
+neural_net = []
+# print(layers_dim)
+
+# Construct the net layer by layer
+for layer_index in range(len(layers_dim)):
+	if layer_index == 0: # if input layer
+		neural_net.append(layer(layer_index, False, 0, layers_dim[layer_index], 'irrelevant'))
+	elif layer_index+1 == len(layers_dim): #if output layer
+		neural_net.append(layer(layer_index, True, layers_dim[layer_index-1], layers_dim[layer_index], activation='linear'))
+	else:
+		neural_net.append(layer(layer_index, False, layers_dim[layer_index-1], layers_dim[layer_index], activation='relu'))
+# print(neural_net)
+
+# Simple check on overfitting
+pred_n_param = sum([(layers_dim[layer_index]+1) * layers_dim[layer_index+1] for layer_index in range(len(layers_dim)-1)])
+act_n_param = sum([neural_net[layer_index].W.size + neural_net[layer_index].b.size for layer_index in range(1,len(layers_dim))])
+print(f'Predicted number of hyperparameters: {pred_n_param}')
+print(f'Actual number of hyperparameters: {act_n_param}')
+print(f'Number of data: {X_num_col}')
+
+if act_n_param >= X_num_col:
+	print('it will overfit')
+	raise Exception('it will overfit.')
